28082bb37b2bf177d5c68ecde7545224ed174811
FoxFireFive <145730403+FoxFireFive0135@users.noreply.github.com>
2026-04-12 21:01:21 -0400

    Add files via upload

---
+++ LogicWorldVoiceChat/src/shared/LWVC_MicrophoneData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using LogicAPI.Networking.Packets;
+using MessagePack;
+
+namespace FoxFireFive_LogicWorldVoiceChat.Shared
+{
+    [MessagePackObject]
+    public sealed class LWVC_MicrophoneData : Packet
+    {
+        [Key(0)]
+        public string sender;
+        [Key(1)]
+        public Vector3 world_pos;
+        [Key(2)]
+        public short[] mic_data;
+    }
+}
